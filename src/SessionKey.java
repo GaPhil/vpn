83c560003f4f236f81e6f577a1e528a119cef97b
GaPhil <gaphil.io@gmail.com>
2018-11-21 15:28:58 +0100

    Add decode method

--- src/SessionKey.java
+++ src/SessionKey.java
@@ -52,4 +52,13 @@ public class SessionKey {
         return encodeToString(this.secretKey.getEncoded());
     }
 
+    /**
+     * Returns Base64 decoded byte array containing the AES key.
+     *
+     * @param encodeKey Base63 encoded key as string
+     * @return key as decoded byte array
+     */
+    public byte[] decodeKey(String encodeKey) {
+        return Base64.getDecoder().decode(encodeKey);
+    }
 }

928026210d9a97d10423c0eced46d6304a66ec23
GaPhil <gaphil.io@gmail.com>
2018-12-09 13:08:34 +0100

    Update test case

--- test/TestCertificateVerification.java
+++ test/TestCertificateVerification.java
@@ -1,7 +1,7 @@
 /**
  * Created by GaPhil on 2018-12-03.
  * <p>
- * Test Certificate Verification with cert_ca.pem and cert_user.pem files.
+ * Test Certificate Verification with cert_ca.pem and cert_server.pem files.
  * Ensure both certificates exist by running:
  * $ sh create_user "Bob Smith bob@smith.com"
  * and
@@ -10,6 +10,7 @@
 public class TestCertificateVerification {
 
     public static void main(String[] args) {
-        VerifyCertificate.verifyCertificate("cert_ca.pem", "cert_user.pem");
+        VerifyCertificate.verifyCertificate("cert_ca.pem", "cert_server.pem");
+        VerifyCertificate.verifyCertificate("cert_ca.pem", "cert_client.pem");
     }
 }

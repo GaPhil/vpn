f68da0bf50e15824c2b22d5685fa6235ae33c45d
GaPhil <gaphil.io@gmail.com>
2018-12-11 14:37:29 +0100

    Add utils

---
+++ src/utils/Logger.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Logger {
+    private static final boolean ENABLE_LOGGING = true;
+
+    public static void log(String message) {
+        if (ENABLE_LOGGING) {
+            System.out.println(message);
+        }
+    }
+}
